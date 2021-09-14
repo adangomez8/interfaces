f134d57032f9d2dde8f28a9d2e4c3d88ee29386c
lujan sasha <lujannatasha1989@gmail.com>
2021-09-14 10:40:15 -0300

    se modifico lienzo y se agregaron funciones del mouse

--- trabajos/TP1/EntregaFinal/js/punto1.js
+++ trabajos/TP1/EntregaFinal/js/punto1.js
@@ -12,8 +12,50 @@ document.addEventListener("DOMContentLoaded", function () {
     let width = canvas.width;
     let height = canvas.height;
     let imageData = ctx.createImageData(width, height);
+
+    let limpiar = document.getElementById("clean");
     let pencil = false;
     let rubber = false;
     let color = "black";
     let lineWidth = 5;
+    let dibujar = false;
+    ctx.lineJoin = "round";
+
+    limpiar.addEventListener('click', function(evt) {
+        dibujar = false;
+        //ctx.clearRect(0, 0, cw, ch);
+ 
+      }, false);
+
+    canvas.addEventListener('mousedown', function (evt) { //el mouse dibuja
+        dibujar = true;
+        //ctx.clearRect(0, 0, cw, ch);
+        ctx.beginPath();
+
+    }, false);
+
+    canvas.addEventListener('mouseup', function (evt) { //se suelta el mouse
+        dibujar = false;
+
+    }, false);
+
+    canvas.addEventListener("mouseout", function (evt) { //el mouse sale
+        dibujar = false;
+    }, false);
+
+    canvas.addEventListener("mousemove", function (evt) { //se mueve el mouse
+        if (dibujar) {
+            let m = oMousePos(canvas, evt);
+            ctx.lineTo(m.x, m.y);
+            ctx.stroke();
+        }
+    }, false);
+
+    function oMousePos(canvas, evt) { //posicion del mouse
+        var ClientRect = canvas.getBoundingClientRect();
+        return { //objeto
+            x: Math.round(evt.clientX - ClientRect.left),
+            y: Math.round(evt.clientY - ClientRect.top)
+        }
+    }
 })
