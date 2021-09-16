443a3ac53220eb489abff9b2db56f8a759f1dd97
lujan sasha <lujannatasha1989@gmail.com>
2021-09-16 11:08:34 -0300

    se agrego funcion del filtro original

--- trabajos/TP1/EntregaFinal/js/filtros.js
+++ trabajos/TP1/EntregaFinal/js/filtros.js
@@ -9,3 +9,18 @@ let g;
 let b;
 let a = 255;
 
+document.getElementById('original').value=original();
+document.getElementById('negativo').value=negative();
+document.getElementById('brillo').value=brightness();
+document.getElementById('saturacion').value=saturation();
+document.getElementById('binario').value=binarization();
+document.getElementById('sepia').value=sepia();
+document.getElementById('blur').value=blur();
+
+function original() {
+    let imagen = new imagen();
+    imagen.src = "";
+    imagen.onload = function() {
+        ctx.drawImagen(imagen, 0, 0);
+    }
+}
