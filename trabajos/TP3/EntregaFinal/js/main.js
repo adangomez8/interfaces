6c57d20e1074b0757b99c3b10694f54c38fd3d46
unknown <lujannatasha1989@gmail.com>
2021-10-26 17:14:37 -0300

    se agrego funcionamiento del juego

--- trabajos/TP3/EntregaFinal/js/main.js
+++ trabajos/TP3/EntregaFinal/js/main.js
@@ -1,54 +1,132 @@
 "use strict";
 
-let mario = document.querySelector("#mario");
 let salta = false;
 let camina = false;
+let morir = false;
 
+let puntos = 0;
+let cronometro = null;
+let interval;
+const MiliSegundos = 1000;
 
+let jugando = false;
+let fin = false;
 
-window.addEventListener("keydown", function (event) {
-    if (event.defaultPrevented) {
-      return; // Do nothing if the event was already processed
-    }
-  
-    switch (event.key) {
-      case "ArrowDown":
-        // code for "down arrow" key press.
-        break;
-      case "ArrowUp":
-        salta = true;
-        saltar(salta);
-        break;
-      case "ArrowLeft":
-        camina = true;
-        caminarParaAtras(camina);
-        break;
-      case "ArrowRight":
-        camina = true;
-        caminar(camina);
-        break;
-      default:
-        return; // Quit when this doesn't handle the key event.
-    }
-  
-    // Cancel the default action to avoid it being handled twice
-    event.preventDefault();
-  }, true);
+let mario = document.querySelector("#mario");
+let tuberia = document.querySelector("#tuberia");
+let moneda = document.querySelector("#moneda");
 
-function saltar(salta){
-    if(salta){
-    mario.setAttribute("class","salta");
-    }
+let fondo = document.getElementById("fondo");
+let planta = document.getElementById("planta");
+let piso = document.getElementById("piso");
+
+let puntaje = document.getElementById("puntos");
+let time = document.getElementById("tiempo");
+let t = document.getElementById("cartel2");
+let exp = document.querySelector("#explicacion");
+
+let btn1 = document.querySelector(".reiniciar");
+btn1.addEventListener("click", reiniciar);
+let btn2 = document.querySelector(".iniciar");
+btn2.addEventListener("click", iniciar);
+
+/*reinicia el juego*/
+function reiniciar() {
+  window.location.reload();
 }
 
-function caminar(camina){
-    if(camina){
-    mario.setAttribute("class","camina");
-    }
+/*inicia el juego*/
+function iniciarJuego() {
+  jugando = true;
+  if (jugando) {
+    reacomodarClases();
+    setInterval(detectarChoque, 500);
+  }
+  tiempoDeJuego();
 }
 
+/*acomoda los elementos necesarios para poder empezaar a jugar*/
+function reacomodarClases() {
+  btn1.classList.remove("iniciar");
+  btn1.classList.add("ocultar");
+  mario.classList.remove("ocultar");
+  moneda.classList.remove("ocultar");
+  tuberia.classList.remove("ocultar");
+  mario.classList.add("camina");
+  moneda.classList.add("moneda");
+  tuberia.classList.add("tuberia");
+  explicacion.classList.add("ocultar");
+}
+
+/*detecta si mario choco contra un obstaculo*/
+function detectarChoque() { }
+
+/*mario salta al presionar la flecha arriba*/
+document.addEventListener("keydown", event => {
+  if (event.code == "ArrowUp") {
+    saltar = true;
+    saltar(salta)
+  }
+});
+
+function saltar(salta) {
+  if (salta) {
+    mario.setAttribute("class", "salta");
+  }
+  else
+    mario.setAttribute("class", "camina");
+}
+mario.addEventListener("animationend", function () { saltar(false) })
+
+/*
 function caminarParaAtras(camina){
     if(camina){
     mario.setAttribute("class","caminaParaAtras");
     }
+}*/
+
+/*muestra lo que queda tiempo para terminar el juego*/
+function tiempoDeJuego() {
+  if (interval != null) {
+    clearInterval(interval);
+  }
+  cronometro = new Tiempo(10, t);
+  if (fin == false) {
+    interval = setInterval(function () {
+      cronometro.calcularTiempo();
+      if (cronometro.calcularTiempo() < 60 && cronometro.calcularTiempo() > 0) {
+        t.setAttribute("class", "tiempoRojo");
+      }
+    }, MiliSegundos);
+  }
 }
+
+/*fin del juego*/
+function finJuego() {
+  jugando = false;
+  fin = true;
+  pausarAnimaciones();
+  clearInterval(interval);
+  cronometro.detenerTiempo();
+  btn2.classList.remove("ocultar");
+  btn2.classList.add("desocultar");
+}
+
+/*pausa las animaciones al terminar el juego*/
+function pausarAnimaciones() {
+
+  mario.style.animationPlayState = "paused";
+  tuberia.style.animationPlayState = "paused";
+  fondo.style.animationPlayState = "paused";
+  planta.style.animationPlayState = "paused";
+  piso.style.animationPlayState = "paused";
+  moneda.style.animationPlayState = "paused";
+
+}
+
+/*va sumando los puntos*/
+function sumarPuntos() {
+  puntos++;
+  puntaje.innerHTML = puntos;
+}
+
