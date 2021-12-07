48c12d8a429f7275f45cc859a18d0d34f3ec5896
unknown <lujannatasha1989@gmail.com>
2021-12-07 16:19:36 -0300

    se agrego busqueda

--- trabajos/TP5/EntregaFinal/js/nav.js
+++ trabajos/TP5/EntregaFinal/js/nav.js
@@ -1,7 +1,9 @@
 "use strict";
 
+let menuBuscar = document.getElementById("menuDeBusquedas");
 let menuDesplegable = document.getElementById("menuDesplegable");
 let desplegable=true;
+let buscar=true;
 
 let perfil = document.getElementById("perfil");
 perfil.addEventListener("click", verPerfil);
@@ -12,6 +14,47 @@ salir.addEventListener("click", cerrarSesion);
 let logoPerfil = document.getElementById("logoPerfil");
 logoPerfil.addEventListener("click", mostrarMenu);
 
+let lupa = document.getElementById("busquedaAvanzada");
+lupa.addEventListener("click", menuAvanzado);
+
+let btnBuscar = document.getElementById("btnBuscar");
+btnBuscar.addEventListener("click", verBusqueda);
+
+let resultado = document.getElementById("resultado");
+
+function menuAvanzado(){
+    if(buscar){
+        menuBuscar.classList.remove("oculto");
+        menuBuscar.classList.add("desocultar");
+        buscar=false;
+        if(resultado.classList.contains("desocultar")){
+            ocultar();
+        }
+    }
+    else{
+        menuBuscar.classList.add("oculto");
+        menuBuscar.classList.remove("desocultar");
+        ocultar();
+    }
+}
+
+function cerrarMenuAvanzado(){
+    menuBuscar.classList.add("oculto");
+    menuBuscar.classList.remove("desocultar");
+    buscar=true;
+    ocultar();
+}
+
+function verBusqueda(){
+    resultado.classList.remove("oculto");
+    resultado.classList.add("desocultar");
+}
+
+function ocultar(){
+    resultado.classList.remove("desocultar");
+    resultado.classList.add("oculto");
+}
+
 function verPerfil(){
     menuDesplegable.classList.add("oculto")
     menuDesplegable.classList.remove("desocultar")
@@ -21,7 +64,7 @@ function verPerfil(){
 function cerrarSesion(){
     menuDesplegable.classList.add("oculto")
     menuDesplegable.classList.remove("desocultar")
-    document.location.href ="index.html";
+    document.location.href ="EntregaFinal/index.html";
 }
 
 function mostrarMenu(){
