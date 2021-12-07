c5849638c57a4909d60715aa7ec281b59d68c7c8
Adan Gomez <adanvgomez1234@gmail.com>
2021-12-07 16:11:21 -0300

    se agrego sector mensajes y parallax

--- trabajos/TP5/EntregaFinal/js/nav.js
+++ trabajos/TP5/EntregaFinal/js/nav.js
@@ -21,7 +21,7 @@ function verPerfil(){
 function cerrarSesion(){
     menuDesplegable.classList.add("oculto")
     menuDesplegable.classList.remove("desocultar")
-    document.location.href ="index.html";
+    document.location.href ="../index.html";
 }
 
 function mostrarMenu(){
