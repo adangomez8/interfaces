1c41208a3c657053c2f0e21b150078275092f31c
unknown <lujannatasha1989@gmail.com>
2021-12-07 13:18:20 -0300

    se agregp cerrar sesion

---
+++ trabajos/TP5/EntregaFinal/js/nav.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let menuDesplegable = document.getElementById("menuDesplegable");
+let desplegable=true;
+
+let perfil = document.getElementById("perfil");
+perfil.addEventListener("click", verPerfil);
+
+let salir = document.getElementById("salir");
+salir.addEventListener("click", cerrarSesion);
+
+let logoPerfil = document.getElementById("logoPerfil");
+logoPerfil.addEventListener("click", mostrarMenu);
+
+function verPerfil(){
+    menuDesplegable.classList.add("oculto")
+    menuDesplegable.classList.remove("desocultar")
+    document.location.href ="../html/perfil-publicaciones.html";
+}
+
+function cerrarSesion(){
+    menuDesplegable.classList.add("oculto")
+    menuDesplegable.classList.remove("desocultar")
+    document.location.href ="index.html";
+}
+
+function mostrarMenu(){
+    if(desplegable){
+        menuDesplegable.classList.remove("oculto");
+        menuDesplegable.classList.add("desocultar");
+        desplegable=false;      
+    }
+    else{
+        menuDesplegable.classList.add("oculto");
+        menuDesplegable.classList.remove("desocultar");
+        desplegable=true;
+    }
+}
